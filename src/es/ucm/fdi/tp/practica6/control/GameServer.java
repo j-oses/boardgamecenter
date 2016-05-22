107697d68d0c18f42ee6d0a9303303a0e77d8b84
Alvaro Rodriguez Garcia <alvarorgtr@gmail.com>
2016-05-22 22:34:09 +0200

    Some javadoc parameters added

--- src/es/ucm/fdi/tp/practica6/control/GameServer.java
+++ src/es/ucm/fdi/tp/practica6/control/GameServer.java
@@ -18,31 +18,45 @@ import java.util.logging.Logger;
 public class GameServer extends AbstractServer implements GameObserver, ServerWindow.ServerChangesListener {
 
 	private static final Logger log = Logger.getLogger(GameServer.class.getSimpleName());
+
 	/**
 	 * Contains the server's UI
 	 */
 	private ServerWindow serverWindow;
+
 	/**
 	 * Controller for the game which will be played
 	 */
 	private Controller controller;
+
 	/**
 	 * The server will communicate with its clients through these SocketEndPoints
 	 */
 	private List<SocketEndpoint> endpoints;
+
 	/**
 	 * The maximum number of connections that the server will accept;
 	 */
 	private int maxConnections;
+
 	/**
 	 * List of the game's pieces
 	 */
 	private List<Piece> pieces;
+
 	/**
 	 * The game's factory, will be sent along the pieces to allow the client to start the game
 	 */
 	private GameFactory factory;
 
+	/**
+	 * Constructs a new game server.
+	 * @param controller the game controller.
+	 * @param pieces the list of pieces.
+	 * @param factory the game factory.
+	 * @param port the port the endpoints will use to connect to the server.
+	 * @param timeout the timeout for the server connection.
+	 */
 	public GameServer(Controller controller, List<Piece> pieces, GameFactory factory, int port, int timeout) {
 		super(port, timeout);
 
@@ -86,6 +100,10 @@ public class GameServer extends AbstractServer implements GameObserver, ServerWi
 	}
 
 	// GAME RELATED METHODS
+
+	/**
+	 * Starts a game
+	 */
 	private void startGame() {
 		controller.start();
 		serverWindow.addLineToStatus("Game started!");
@@ -133,8 +151,8 @@ public class GameServer extends AbstractServer implements GameObserver, ServerWi
 	// CONNECTION HELPING METHODS
 
 	/**
-	 * Notifies the clients (through the endPoints) that something(message) has happened.
-	 * @param message
+	 * Notifies the clients (through the endPoints) that something (message) has happened.
+	 * @param message the message to be sent.
      */
 	private void notifyEndpoints(NotificationMessage message) {
 		for (SocketEndpoint endpoint : endpoints) {
@@ -143,8 +161,8 @@ public class GameServer extends AbstractServer implements GameObserver, ServerWi
 	}
 
 	/**
-	 * A message sent to the endpoints to tell them that the connection has been established
-	 * @param endpoint
+	 * Saves the connection and starts the game if there are enough sockets.
+	 * @param endpoint the endpoint whose connection was just established.
 	 */
 	private void connectionEstablishedToEndpoint(SocketEndpoint endpoint) {
 		if (endpoints.size() < maxConnections) {
@@ -164,8 +182,8 @@ public class GameServer extends AbstractServer implements GameObserver, ServerWi
 	}
 
 	/**
-	 * A message sent  at the start to the clients used exclusively to start and set up the game
-	 * @param endpoint
+	 * A message sent at the start to the clients used exclusively to start and set up the game.
+	 * @param endpoint the endpoint to which the message will be sent.
 	 */
 	private void sendStartupInfoToEndpoint(SocketEndpoint endpoint) {
 		ConnectionEstablishedMessage message = new ConnectionEstablishedMessage(pieces.get(endpoints.size() - 1),
